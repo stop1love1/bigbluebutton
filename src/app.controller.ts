c71b4ad8585a24bd85d2d695bb39b4860583fcf5
Long <nlong2002f@gmail.com>
2024-10-28 15:20:29 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { ApiQuery } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,14 +6,14 @@ export class AppController {
     constructor(private readonly appService: AppService) {}
 
     @Get('directories')
-    @ApiQuery({
-        name: 'path',
-        type: String,
-        required: false,
-    })
-    getSubdirectories(@Query('name') name?: string): Promise<any> {
+    getSubdirectories(@Query('name') name: string): Promise<any> {
         return this.appService.getSubdirectories(
             name ? `${process.env.LEARNING_DASHBOARD_PATH}/${name}` : process.env.LEARNING_DASHBOARD_PATH,
         );
+    }
+
+    @Get('file-content')
+    async readFileContent(@Query('filePath') filePath: string): Promise<string | null> {
+        return this.appService.readFileContent(filePath);
     }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -10,10 +10,21 @@ export class AppService {
             const directories = await fs.promises.readdir(name, {
                 withFileTypes: true,
             });
-            const subdirectories = directories.filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name);
+            const subdirectories = directories
+                .filter((dirent: any) => dirent.isDirectory())
+                .map((dirent: any) => dirent.name);
             return subdirectories;
         } catch {
             return [];
+        }
+    }
+
+    async readFileContent(filePath: string): Promise<string | null> {
+        try {
+            const content = await fs.promises.readFile(`${process.env.LEARNING_DASHBOARD_PATH}/${filePath}`, 'utf-8');
+            return content;
+        } catch (error) {
+            throw error;
         }
     }
 }
